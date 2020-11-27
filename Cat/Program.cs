1d55b191127114ae405b8cfefc93ef5e1669c41a
Opilane TTHK <opilane@tthk.ee>
2020-11-27 09:58:16 +0200

    first commit

---
+++ Cat/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Cat
+{
+    class Program
+    {
+        class Cat
+        {
+            string name;
+            double hunger;
+            string color;
+
+            public Cat(string _color, string _name)
+            {
+                color = _color;
+                name = _name;
+                hunger = 0;
+            }
+            public string Name
+            {
+                get { return name; }
+            }
+            public double Hunger
+            {
+                get { return hunger; }
+            }
+
+            public string Color
+            {
+                get { return Color; }
+            }
+
+            public void sleep()
+            {
+                Console.WriteLine("Cat sleeping!");
+                Console.WriteLine("Hunger:" + hunger);
+                hunger += 0.2;
+            }
+
+            public void Meow()
+            {
+                Console.WriteLine("meow!!");
+                
+            }
+
+        }
+
+        
+        
+        static void Main(string[] args)
+        {
+            Cat newCat = new Cat("Kitty", "Orange");
+            Console.WriteLine(newCat.Hunger);
+            while (newCat.Hunger!= 1)
+            {
+                newCat.sleep();
+            }
+        }
+    }
+}
